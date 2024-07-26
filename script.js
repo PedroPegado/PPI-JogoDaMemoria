45814484e397ad19353d89f7e758043f7e110c92
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2024-07-26 02:21:15 -0300

    começo

---
+++ script.js
@@ -0,0 +1,23 @@
+function shuffle(){
+
+}
+
+function createBoard(){
+    
+}
+
+function flipCard(){
+    
+}
+
+function checkForMatch(){
+    
+}
+
+function unflipCards(){
+    
+}
+
+function resetCards(){
+    
+}
