5aea86dda4eb92a6f5996161de48e2bec1d4af8e
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2024-07-29 17:54:28 -0300

    começando a fazer

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-function shuffle(){
+const countries = ['br', 'ar', 'cl', 'cu', 'gt', 'ht', 'mx', 'ca']
 
+function shuffle(countries) {
+    for (let i = countries.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [countries[i], countries[j]] = [countries[j], countries[i]];
+    }
 }
 
-function createBoard(){
-    
+function createBoard() {
+    const divs = document.querySelectorAll('.game-board .card .img-container .image-card')
+    let items = [...countries, ...countries]
+    shuffle(items)
+
+    divs.forEach((div, index) => {
+        const countryCode = items[index];
+        const img = document.createElement('img');
+        img.src = `paises/${countryCode}.svg`;
+        img.alt = countryCode;
+        div.innerHTML = '';
+        div.appendChild(img);
+    });
 }
 
 function flipCard(){
-    
+    const card = document.getElementById('Card')
+    card.classList.remove('flipped')
 }
 
 function checkForMatch(){
@@ -21,3 +38,4 @@ function unflipCards(){
 function resetCards(){
     
 }
+
