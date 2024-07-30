12cd9352ff47fb609bd010fc6a1b2a77f57482ce
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2024-07-30 18:54:32 -0300

    consegui completar todas as funções

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const countries = ["br", "ar", "cl", "gt", "cu", "ht", "mx", "ca", "eg", "za", "ng", "ke", "ma", "gh", "et", "ao", "de", "fr", "it", "es", "lv", "gr", "is", "se", "jp", "cn", "in", "kr", "kp", "th", "my", "bt"]
 
+let check = []
+
 function shuffle(countries) {
     for (let i = countries.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -8,46 +10,88 @@ function shuffle(countries) {
 }
 
 function createBoard() {
-    const cards = document.querySelectorAll('.game-board .card .img-container .image-card')
-    const card_container = document.getElementById('Card')
-    let selectedCountries = countries.slice(0, cards.length / 2);
-    let items = [...selectedCountries, ...selectedCountries];
+    const gameBoard = document.getElementById('gameBoard')
+    gameBoard.innerHTML = ''
+
+    let selectedCountries = countries.slice(0, 32)
+    let items = [...selectedCountries, ...selectedCountries]
     shuffle(items)
 
-    cards.forEach((card, index) => {
-        card_container.classList.add('flipped')
-        const countryCode = items[index];
+    items.forEach((countryCode, index) => {
+        const card = document.createElement('button')
+        card.className = 'card flipped'
+        card.id = `Card-${index+1}`
+        card.setAttribute('onclick', 'flipCard()')
+
+        const frontDiv = document.createElement('div')
+        frontDiv.className = 'front'
+
+
+        const imgContainer = document.createElement('div')
+        imgContainer.className = 'img-container'
+
+
+        const imageCard = document.createElement('div')
+        imageCard.className = 'image-card'
+
+
         const img = document.createElement('img')
         img.src = `paises/${countryCode}.svg`
         img.className = 'image-card-inside'
         img.id = `ImageCard-${index+1}`
-        card.appendChild(img)
-    });
+
+        imageCard.appendChild(img)
+        imgContainer.appendChild(imageCard)
+        card.appendChild(frontDiv)
+        card.appendChild(imgContainer)
+        gameBoard.appendChild(card)
+
+        card.addEventListener('click', () => {
+            flipCard(card)
+        })
+    })
 }
 
-function flipCard(){
-    const card = document.getElementById('Card')
-    card.classList.remove('flipped')
+function flipCard(cardElement) {
+    cardElement.classList.toggle('flipped')
+    
+    const img = cardElement.querySelector('.image-card-inside')
+
+    if (img) {
+        check.push(img.src)
+        check.push(cardElement.id)
+        console.log(check)
+    }
+
+    if (check.length == 4) {
+        checkForMatch()
+    }
 }
 
 function checkForMatch(){
-    
+    if (check[0] == check[2]) {
+        console.log('são iguais')
+        check = []
+    }
+    else {
+        console.log('diferentes')
+        unflipCards();
+        check = []
+    }
 }
 
 function unflipCards(){
-    
+    card_1 = document.getElementById(check[1])
+    card_2 = document.getElementById(check[3])
+
+    setTimeout(() => {
+        card_1.classList.toggle('flipped');
+        card_2.classList.toggle('flipped');
+    }, 500);
 }
 
 function resetCards(){
-    const cards_image = document.querySelectorAll('.game-board .card .img-container .image-card .image-card-inside')
-
-    cards_image.forEach((image, index) => {
-        img = document.getElementById(`ImageCard-${index+1}`)
-        img.remove();
-    })
-
     createBoard()
 }
 
-createBoard()
-shuffle()
+document.addEventListener('DOMContentLoaded', createBoard);
